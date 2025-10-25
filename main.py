ed44456d2c346590d159ab2f7a79b431b97f5c3a
Brian Tuohy <BTcodez22@gmail.com>
2025-10-24 19:11:26 -0500

    b: get word counts

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("greetings boots")
+def get_book_text():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        #print(file_contents)
+        return file_contents
+    
+#def main():
+#   text = get_book_text()
+#   return text
+
+def count_words():
+    text = get_book_text()
+    #print(text)
+    words = text.split()
+    num_words = len(words)
+    print(f"Found {num_words} total words")
+    
+count_words()
+    
+
+
+
+    
