1d946b766550aa670466a97d61ced924cf5604bf
Brian Tuohy <BTcodez22@gmail.com>
2025-10-29 12:33:54 -0500

    d: final comp, access books from user cli or return error

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import sys
+
 from stats import get_book_text
 from stats import count_words
 from stats import count_chars
 from stats import sort_chars
 
+
 def main():
-    book_path = "books/frankenstein.txt"
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = count_words(text)
     char_count = count_chars(text)
@@ -18,8 +26,7 @@ def main():
     for i in sorted_chars:
         print(f"{i['char']}: {i['num']}")
     print("============= END ===============")
-
-    
+   
 main()
     
 
--- stats.py
+++ stats.py
@@ -7,10 +7,10 @@ def count_words(text):
     #print(text)
     words = text.split()
     return len(words)
+ 
     
 def count_chars(text):
     char_count = {}
-
     for char in text:
         low_char = char.lower()
         if low_char not in char_count:
@@ -18,7 +18,8 @@ def count_chars(text):
         else:
             char_count[low_char] += 1
     return char_count   
-    
+
+
 def sort_chars(char_count):
     char_org = [{'char': key, 'num': value} for key, value in char_count.items() if key.isalpha()]
     def sort_nums(num):
